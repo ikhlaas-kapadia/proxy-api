490c0551befdc8470d7f0cc5448c821c8776594f
ikhlaas <user@Users-MacBook-Pro.local>
2022-12-05 16:01:49 +0000

    Update test file

--- test/api.test.js
+++ test/api.test.js
@@ -2,10 +2,10 @@ const request = require('supertest');
 
 const app = require('../src/app');
 
-describe('GET /api/v1', () => {
+describe('GET /api/v2', () => {
   it('responds with a json message', (done) => {
     request(app)
-      .get('/api/v1')
+      .get('/api/v2')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200, {
@@ -14,10 +14,10 @@ describe('GET /api/v1', () => {
   });
 });
 
-describe('GET /api/v1/emojis', () => {
+describe('GET /api/v2/emojis', () => {
   it('responds with a json message', (done) => {
     request(app)
-      .get('/api/v1/emojis')
+      .get('/api/v2/emojis')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200, ['😀', '😳', '🙄'], done);
