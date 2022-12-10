16a15266d41868a97e58e83d3e41dc12c80ccd36
ikhlaas <user@Users-MacBook-Pro.local>
2022-12-10 17:44:59 +0000

    Add extra params and query handlers

--- src/api/crypto.js
+++ src/api/crypto.js
@@ -26,6 +26,7 @@ let cachedDataStats;
 let cacheTimeStats;
 
 router.get('/stats', limiter, async (req, res, next) => {
+
   const options = {
     method: 'GET',
     url: `${baseURL}/stats`,
@@ -55,9 +56,16 @@ router.get('/stats', limiter, async (req, res, next) => {
 
 router.get('/coins', limiter, async (req, res, next) => {
 
+  
+  const queryObject = req.query;
+  let queryString = ''
+  for (const [key, value] of Object.entries(queryObject)) {
+    let string = `?${key}=${value}`;
+    queryString = string;
+  }
   const options = {
     method: 'GET',
-    url: `${baseURL}/coins`,
+    url: `${baseURL}/coins${queryString}`,
     headers: {
       'x-access-token': process.env.RAPID_API_KEY
     }
@@ -87,8 +95,6 @@ router.get('/coin/:uuid', limiter, async (req, res, next) => {
   const { params:paramsObject } = req;
   const { uuid } = paramsObject;
 
-  console.log(uuid);
-
   const options = {
     method: 'GET',
     url: `${baseURL}/coin/${uuid}`,
@@ -113,12 +119,12 @@ router.get('/coin/:uuid', limiter, async (req, res, next) => {
 router.get('/coin/:uuid/history', limiter, async (req, res, next) => {
   const { params:paramsObject, query:queryObject } = req;
   const { uuid } = paramsObject;
-  const { timePeriod } = queryObject;
   let queryString = ''
-  if(timePeriod) {
-    queryString = `?timePeriod=${timePeriod}`;
+  for (const [key, value] of Object.entries(queryObject)) {
+    let string = `?${key}=${value}`;
+    queryString = string;
   }
-
+ 
   const options = {
     method: 'GET',
     url: `${baseURL}/coin/${uuid}/history${queryString}`,
@@ -131,7 +137,6 @@ router.get('/coin/:uuid/history', limiter, async (req, res, next) => {
   try {
     const response = await axios.request(options);
     const { data } = await response;
-    console.log(data.data.history.length);
     //  2 - Respond to incoming request from front end with response from rapid api
     return res.json(data);
   } catch (error) {
